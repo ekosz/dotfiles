ca102bcdf6493159591a4146f4c98066949f1947
Eric Koslow <ekoslow@gmail.com>
2013-04-16 15:55:14 -0500

    Initial Commit

---
+++ zsh/koslow/koslow.plugin.zsh
@@ -0,0 +1,13 @@
+
+# ls
+alias ls="ls -FG"
+alias l="ls -lAhG"
+alias ll="ls -lG"
+alias la='ls -AG'
+
+# MySQL
+alias mysqlstart='sudo /opt/local/share/mysql5/mysql/mysql.server start'
+alias mysqlstop='/opt/local/bin/mysqladmin5 -u root -p shutdown'
+
+# Games
+alias crawl='/Applications/Dungeon\ Crawl\ Stone\ Soup\ -\ Console.app/Contents/Resources/crawl ; exit;'
