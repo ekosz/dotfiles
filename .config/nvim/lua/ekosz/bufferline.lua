512257a7f078da67b417a610fb5ca7118c421773
Eric Koslow <ekoslow@gmail.com>
2022-01-08 08:37:44 -0800

    feat(bufferline): Allow for dedup names

--- .config/nvim/lua/ekosz/bufferline.lua
+++ .config/nvim/lua/ekosz/bufferline.lua
@@ -32,7 +32,7 @@ bufferline.setup {
     --   end
     -- end,
     max_name_length = 30,
-    max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
     tab_size = 21,
     diagnostics = false, -- | "nvim_lsp" | "coc",
     diagnostics_update_in_insert = false,
@@ -57,14 +57,14 @@ bufferline.setup {
     -- end,
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     show_buffer_icons = true,
-    show_buffer_close_icons = true,
+    show_buffer_close_icons = false,
     show_close_icon = false,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-    enforce_regular_tabs = true,
+    enforce_regular_tabs = false,
     always_show_bufferline = true,
     -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
     --   -- add custom logic
