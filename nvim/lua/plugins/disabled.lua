ad13821297c06727d8a494e6e62971410677d9f2
Eric Koslow <ekoslow@gmail.com>
2024-09-21 16:05:54 -0200

    latest updates

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "ggandor/flit.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
